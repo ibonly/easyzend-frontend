7ed787e3f5d4a25c6d4fda2f5a6d03f47d1f861f
Adeniyi Ibraheem <7251019+ibonly@users.noreply.github.com>
2025-02-27 23:00:27 -0600

    testing

---
+++ component/Pages/profile.js
@@ -0,0 +1,8 @@
+export default {
+    props: ['transactions'],
+    template: `
+      <div class="profile-page">
+        <h2>Profile</h2>
+      </div>
+    `
+  };
---
+++ component/Pages/setting.js
@@ -0,0 +1,8 @@
+export default {
+    props: ['transactions'],
+    template: `
+      <div class="setting-page">
+        <h2>account Settings</h2>
+      </div>
+    `
+  };
